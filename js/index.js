2a112c4d3496ee1b038ac573ed272d991eb99e51
Josh Bowman <10843511@my.uvu.edu>
2020-04-26 19:48:57 -0600

    animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Custom JS here
+
+let twist = document.querySelector(".logo");
+
+twist.addEventListener('click', event => {
+    gsap.to(".logo, .menu",{
+        duration: 5,
+        rotation: 720        
+      }); 
+  });
+
+
+  let messVisible = document.querySelector(".messType");
+
+  messVisible.addEventListener('keydown', event => {
+    console.log('is it workin');
+    gsap.to(".messBut",{
+        duration: 5,
+        opacity: 1        
+      }); 
+  });
+
